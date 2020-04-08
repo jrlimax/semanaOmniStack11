eb5d08eb5ab08d83c02baecf5c19a44bb5cddf6e
jrlima03 <juninho_lima2020@hotmail.com>
2020-04-08 13:44:54 -0300

    aplicação Mobile

---
+++ mobile/App.js
@@ -0,0 +1,33 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+   <Routes />
+  );
+}
+
+
+
+
+/*
+Exemplo de estilização.
+--------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title:{
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+});*/
